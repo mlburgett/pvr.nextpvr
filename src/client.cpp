85431dbc3267a367428be01b46593eb25200bffa
Alwin Esch <alwin.esch@web.de>
2016-12-18 16:29:51 +0100

    Remove no more used 'void ADDON_Stop()'

--- src/client.cpp
+++ src/client.cpp
@@ -256,11 +256,6 @@ ADDON_STATUS ADDON_SetSetting(const char *settingName, const void *settingValue)
   return ADDON_STATUS_OK;
 }
 
-void ADDON_Stop()
-{
-  ADDON_Destroy();
-}
-
 void ADDON_FreeSettings()
 {
 
