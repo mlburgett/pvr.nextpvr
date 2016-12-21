13dc55a7aa84d4ce67a06d1e9cbecf68da3c0919
Alwin Esch <alwin.esch@web.de>
2016-12-21 21:08:24 +0100

    Change GetAddonCapabilities to 'void GetCapabilities'

--- src/client.cpp
+++ src/client.cpp
@@ -290,10 +290,10 @@ void OnPowerSavingDeactivated()
 {
 }
 
-//-- GetAddonCapabilities -----------------------------------------------------
+//-- GetCapabilities -----------------------------------------------------
 // Tell XBMC our requirements
 //-----------------------------------------------------------------------------
-PVR_ERROR GetAddonCapabilities(PVR_ADDON_CAPABILITIES *pCapabilities)
+void GetCapabilities(PVR_ADDON_CAPABILITIES *pCapabilities)
 {
   XBMC->Log(LOG_DEBUG, "->GetProperties()");
 
@@ -310,8 +310,6 @@ PVR_ERROR GetAddonCapabilities(PVR_ADDON_CAPABILITIES *pCapabilities)
   pCapabilities->bSupportsChannelScan        = false;
   pCapabilities->bSupportsLastPlayedPosition = true;
   pCapabilities->bSupportsRecordingEdl       = true;
-
-  return PVR_ERROR_NO_ERROR;
 }
 
 PVR_ERROR GetStreamProperties(PVR_STREAM_PROPERTIES *pProperties)
